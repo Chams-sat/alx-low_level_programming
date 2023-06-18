7b0843a451e288eba3c09e5b5bfcd498709a4e8d
Chams <shamssatour14@gmail.com>
2023-06-18 08:43:07 -0700

    commits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main Entry point
+ *
+ * Description: print all possible deff combination of two digit
+ *
+ * Return: O
+*/
+
+int main(void)
+{
+	int firstDigit = 0, secondDigit;
+
+	while (firstDigit <= 99)
+	{
+		secondDegit = firstDigit;
+		while (secondDigit <= 99)
+		{
+			if (secondDigit != firstDigit)
+			{
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit % 10) + 48);
+				putchar(' ');
+				putchar((secondDigit / 10) + 48);
+				putchar((secondDigit % 10) + 48);
+
+				if (firstDigit != 98 || secondDigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			secondDigit++;	
+		}
+		firstDigit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
