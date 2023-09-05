fe91a3ac84dc4493a24a6da8de7f7fe1142cb3f2
Chams <shamssatour14@gmail.com>
2023-09-05 08:47:13 -0700

    Updates

---
+++ 0x15-file-io/100-elf_header.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <elf.h>
+
+void print_osabi_more(Elf64_Ehdr h);
+
+/**
+ * print_magic - prints ELF magic bytes
+ * @h: the ELF header struct
+*/
+
+void print_magic(Elf64_Ehdr h)
+{
+	int i;
+
+	printf("  Magic:  ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%2.2x%s", h.e_ident[i], i == EI_NIDENT - l ? "\n" : " ");
+}
+
+/**
+ * print_class - prints ELF class
+ * @h: the ELF header struct
+*/
+
+void print_class(Elf64_Ehdr h)
+{
+	printf("  Class:				");
+	switch (h.e_ident[EI_CLASS])
+	{
+		case ELFCLASS64:
+			printf("ELF64");
+		break;
